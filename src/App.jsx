b585091e24259e7a0b97ea7a585473db81ada5fa
VibeSyntax <138536097+beza102@users.noreply.github.com>
2025-10-28 14:14:58 -0700

    add in the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import {useState} from "react"
+import {useEffect} from "react"
+import Item from "./components/Item";
 export default function App() {
-  return(
-    <h3>Testing</h3>
-  )
+  const [countries, setCountries] = useState([])
+const ENDPOINT ="https://restcountries.com/v3.1/independent?status=true&fields=languages,capital"
+
+useEffect(() => {
+    fetch(ENDPOINT) 
+      .then(res => res.json())
+      console.log(countries)
+      .then(data => {
+        setCountries(data)
+        console.log(data)
+        
+      })
+    }, []);
+
+    
+    
+return(
+  <>
+  {countries.map((item, index) =>{
+    return(
+      <div key={index} className="index">
+        <p>{item.capital}</p>
+      </div>
+    )
+  })}
+  </>
+)
 }
