11337489f67d5f630371ae2a672ee004d385ffd1
ShawnN003 <nguru.shawn@student.greenriver.edu>
2025-10-28 16:54:07 -0500

    phase 1 almost complete!

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,35 @@
 import {useState} from "react"
 import {useEffect} from "react"
 import Item from "./components/Item";
+
 export default function App() {
   const [countries, setCountries] = useState([])
-const ENDPOINT ="https://restcountries.com/v3.1/independent?status=true&fields=languages,capital"
+const ENDPOINT ="https://restcountries.com/v3.1/all?fields=name,capital,currencies"
 
 useEffect(() => {
     fetch(ENDPOINT) 
       .then(res => res.json())
-      console.log(countries)
       .then(data => {
+        console.log(data);
         setCountries(data)
-        console.log(data)
-        
       })
     }, []);
 
-    
+    function region(prop){
+
+    }
     
 return(
   <>
-  {countries.map((item, index) =>{
-    return(
-      <div key={index} className="index">
-        <p>{item.capital}</p>
-      </div>
-    )
-  })}
-  </>
+      {countries.map((item, index) =>{
+        {item.name}
+        return(
+          <div key={index} className="index">
+            
+            <li>{"Region: " + item.name.common + "Capital: "+item.capital}</li>
+    
+          </div>
+        )
+      })}  </>
 )
 }
